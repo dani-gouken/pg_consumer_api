e9ae8745abf048f6efdaab08e65ea772410df199
Nghokeng Daniel <daniel@ycode.com>
2023-12-13 13:38:58 +0100

    service image as asset

--- app/Http/Resources/ServiceResource.php
+++ app/Http/Resources/ServiceResource.php
@@ -15,7 +15,7 @@ class ServiceResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            "image" => $this->image,
+            "image" => \Vite::asset($this->image),
             "name" => $this->name,
             "description" => $this->description,
             "kind" => $this->kind,
