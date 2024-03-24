a3d6395abf3df83f115b864195f1d85ba4c857e1
Nghokeng Daniel <daniel@ycode.com>
2024-03-24 17:10:31 +0100

    fix invalid phone number format

--- app/Services/PaymentGatewayService.php
+++ app/Services/PaymentGatewayService.php
@@ -76,7 +76,7 @@ class PaymentGatewayService implements TransactionServiceInterface, HandlesCallb
             "publicKey" => $this->publicKey,
             "schema_type" => $transaction->service->getKind() == ServiceKindEnum::payment ? "CM_MOBILE_MONEY_SCHEMA" : "PHONE_NUMBER",
             "schema" => [
-                "phoneNumber" => "237{$transaction->destination}"
+                "phoneNumber" => $transaction->service->getKind() == ServiceKindEnum::payment ? "237{$transaction->destination}" : $transaction->destination
             ]
         ]);
 
